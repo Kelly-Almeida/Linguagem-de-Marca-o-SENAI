7dbed267fed9583d581fcc66cf88e74abbae7bc6
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-04-14 16:01:23 -0300

    Menu Mobile

--- Pizzaria/CadEstabelecimento/script.js
+++ Pizzaria/CadEstabelecimento/script.js
@@ -1,8 +1,17 @@
 function abrirMenu(){
+    nav = document.getElementById("navegador");
     menu = document.getElementById("menu");
+    iconeMenu = document.getElementById("menu-mobile");
     
 
     menu.style.display ="flex";
+    iconeMenu.style.display = "none";
 
+    nav.style.top = "100%";
+    nav.style.right = "0%";   
+    nav.style.backgroundColor = "rgba(255, 255, 255)";
+    nav.style.width = "50%";
+    nav.style.padding = "20px";
+    
 
 }
