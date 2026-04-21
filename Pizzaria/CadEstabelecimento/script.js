d585095e63b1b97466a135943c30ac9f9214ed4f
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-04-21 15:40:18 -0300

    criação do CadPagamento

--- Pizzaria/CadEstabelecimento/script.js
+++ Pizzaria/CadEstabelecimento/script.js
@@ -4,17 +4,29 @@ function abrirMenu(){
     iconeMenu = document.getElementById("menu-mobile");
     iconeFechar = document.getElementById("fechar-mobile");
     
-
     menu.style.display ="flex";
     iconeMenu.style.display = "none";
-    iconeFechar.style.display ="flex";
+    iconeFechar.style.display = "flex";
 
+    nav.style.transition = "1s";
     nav.style.top = "100%";
-    nav.style.right = "0%";   
+    nav.style.right = "-2%";   
     nav.style.backgroundColor = "rgba(255, 245, 245, 0.86)";
-    nav.style.width = "50%";
+    nav.style.width = "20%";
     nav.style.padding = "20px";
     nav.style.paddingTop = "10%"
      
+
+}
+
+function fecharMenu(){
+    
+    nav.style.top = "-25%";
+    iconeMenu.style.display = "flex";
+    nav.style.backgroundColor = "transparent";
+    nav.style.transition = "0.4s";
+    menu.style.display = "none";
+
+    
 
 }
