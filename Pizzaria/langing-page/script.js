764732970fe10482071fa852fa1f0538117bc502
Kelly <Kellydealmeidasantos237@gmail.com>
2026-05-20 12:38:56 -0300

    landing page finalizada na versão mobile

--- Pizzaria/langing-page/script.js
+++ Pizzaria/langing-page/script.js
@@ -1,34 +1,18 @@
 function abrirMenu(){
-    nav = document.getElementById("navegador-vertical");
-    navMobile = document.getElementsByClassName("nav-vertical");
-    menu = document.getElementById("menu-vertical");
-    iconeMenu = document.getElementById("menu-mobile");
-    iconeFechar = document.getElementById("fechar-mobile");
-    iconesNav = document.getElementsByClassName("itemMenuVertical");
+    const nav = document.getElementById("navegador-vertical");
+    const iconeMenu = document.getElementById("menu-mobile");
     
-    menu.style.display ="flex";
-    menu.style.backgroundColor = "rgba(255, 245, 245, 0.66)";
-    menu.style.flexDirection = "column";
     iconeMenu.style.display = "none";
-    iconeFechar.style.display = "flex";
-    iconesNav.style.display = "flex";
-    
-    
-
-    nav.style.transition = "1s";
-    nav.style.top = "100%";
-    nav.style.right = "-2%";   
-    nav.style.width = "20%";
-    nav.style.padding = "20px";
-    nav.style.paddingTop = "10%";     
+    nav.classList.add("active");
 
 }
 
 function fecharMenu(){
     
+    const nav = document.getElementById("navegador-vertical");
+    const iconeMenu = document.getElementById("menu-mobile");
+
+    nav.classList.remove("active");
     iconeMenu.style.display = "flex";
-    nav.style.backgroundColor = "transparent";
-    nav.style.transition = "0.4s";
-    menu.style.display = "none";
 
 }
