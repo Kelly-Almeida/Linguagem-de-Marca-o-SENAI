24f17ddb4b235fb4607faa3bfb56362d20cb068f
KELLY DE ALMEIDA SANTOS <kelly.asantos14@senacsp.edu.br>
2026-06-08 17:37:51 -0300

    Entrada de dados ok

---
+++ aulaJS/script.js
@@ -0,0 +1,14 @@
+function verDados(){
+    const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
+    const curso = document.getElementById("curso").value;
+    
+    var notas = [
+        document.getElementById("nota1").value, 
+        document.getElementById("nota2").value, 
+        document.getElementById("nota3").value, 
+        document.getElementById("nota4").value
+    ];
+
+    
+}
