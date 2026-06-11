6a4ac33e01f8b6ecc02803bd70e713369e6e0b0c
Kelly <Kellydealmeidasantos237@gmail.com>
2026-06-11 09:55:44 -0300

    Sistema de cadastro de aluno com JS feito

--- aulaJS/script.js
+++ aulaJS/script.js
@@ -1,14 +1,41 @@
 function verDados(){
+    var soma = 0;
+    const tabela = document.getElementById("info");
+    const form = document.getElementById("formulario");
     const nome = document.getElementById("nome").value;
     const idade = document.getElementById("idade").value;
     const curso = document.getElementById("curso").value;
     
     var notas = [
-        document.getElementById("nota1").value, 
-        document.getElementById("nota2").value, 
-        document.getElementById("nota3").value, 
-        document.getElementById("nota4").value
+        parseFloat(document.getElementById("nota1").value), 
+        parseFloat(document.getElementById("nota2").value), 
+        parseFloat(document.getElementById("nota3").value), 
+        parseFloat(document.getElementById("nota4").value)
     ];
 
+    notas.forEach((nota) => {
+        soma += nota;
+    });
     
+
+    var media = soma/ notas.length;
+
+    form.style.display = "none";
+
+    tabela.innerHTML = `
+    <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+        <th>Curso</th>
+        <th>Média</th>
+    </tr>
+
+    <tr>
+        <td>${nome}</td>
+        <td>${idade}</td>
+        <td>${curso}</td>
+        <td>${media}</td>
+    </tr>
+    `
+
 }
